9ed17659968068af9f5e73b045c6da3fd54d5f3b
Femi Adigun <babaphemy@gmail.com>
2023-02-17 12:14:25 -0600

    testing husky

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,17 +1,54 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { screen, render, act } from "@testing-library/react";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/material";
 const muiTheme = {};
+
 describe("App", () => {
-  it("renders a ThemeProvider with the `muiTheme` and Routes with a Route to the Home component", () => {
-    const { getByTestId } = render(<App />);
-    const themeProvider = getByTestId("theme-provider");
-    expect(themeProvider).toBeInTheDocument();
+  beforeEach(() => {
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () => Promise.resolve([{ id: 1, email: "femi" }]),
+      })
+    ) as jest.Mock;
+  });
 
-    const routes = getByTestId("routes");
-    expect(routes).toBeInTheDocument();
-
-    const route = getByTestId("route-home");
-    expect(route).toBeInTheDocument();
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  it("renders without breaking", () => {
+    const { container } = render(
+      <BrowserRouter>
+        <ThemeProvider theme={muiTheme}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    );
+    expect(container).toBeDefined();
+  });
+  it("renders the H1 tag", () => {
+    const { getByText } = render(
+      <BrowserRouter>
+        <ThemeProvider theme={muiTheme}>
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    );
+    const txtH1 = getByText("Horace Learning");
+    expect(txtH1).toBeInTheDocument();
+  });
+  it("renders the email", async () => {
+    await act(async () =>
+      render(
+        <BrowserRouter>
+          <ThemeProvider theme={muiTheme}>
+            <App />
+          </ThemeProvider>
+        </BrowserRouter>
+      )
+    );
+    const txtEmail = screen.getAllByText("femi");
+    expect(txtEmail.length).toBe(1);
   });
 });
---
+++ src/hooks/useAuth.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import { getUsers } from "../api/rest";
+
+const useAuth = () => {
+  const [users, setUsers] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  useEffect(() => {
+    setLoading(true);
+    let contr = new AbortController();
+    async function aUsers() {
+      const all = await getUsers(contr.signal);
+      setUsers(all);
+      setLoading(false);
+    }
+    aUsers();
+    return () => {
+      contr.abort();
+    };
+  }, []);
+
+  return [users, loading];
+};
+
+export default useAuth;
