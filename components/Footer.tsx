ba9c9efcdfa99e7cccf8c63a535b76d9bb6850c7
Isah Ibrahim <isahibn08@gmail.com>
2023-02-18 12:10:58 +0100

    lint prettier

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React from 'react';
+import { Container, Box, Typography, Grid } from '@mui/material';
 
 const Footer = () => {
   return (
-    <div>
-      <p>Footer</p>
-    </div>
+    <Box sx={footerStyles.container}>
+      <Container maxWidth="lg">
+        <Grid container spacing={5}>
+          <Grid item sm={12} md={3}>
+            <Typography variant="h6" gutterBottom>
+              Company Name
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </Box>
   );
 };
 
 export default Footer;
+
+const footerStyles = {
+  container: {
+    marginTop: 5
+  }
+};
