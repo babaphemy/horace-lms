ae3951096d01e9627666e7ccda5d5e2e50397503
Femi Adigun <babaphemy@gmail.com>
2023-02-24 18:02:31 -0600

    chore:00 adding tailwind

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -4,19 +4,19 @@ import { getUsers } from '../api/rest';
 const useAuth = () => {
 	const [users, setUsers] = useState<any>([]);
 	const [loading, setLoading] = useState<boolean>(false);
-	useEffect(() => {
-		setLoading(true);
-		let contr = new AbortController();
-		async function aUsers() {
-			const all = await getUsers(contr.signal);
-			setUsers(all);
-			setLoading(false);
-		}
-		aUsers();
-		return () => {
-			contr.abort();
-		};
-	}, []);
+	// useEffect(() => {
+	// 	setLoading(true);
+	// 	let contr = new AbortController();
+	// 	async function aUsers() {
+	// 		const all = await getUsers(contr.signal);
+	// 		setUsers(all);
+	// 		setLoading(false);
+	// 	}
+	// 	aUsers();
+	// 	return () => {
+	// 		contr.abort();
+	// 	};
+	// }, []);
 
 	return [users, loading];
 };
